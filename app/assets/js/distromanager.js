803e98589c0e420a235474f37087f756be7ef98d
AxenoDev <tbavart@gmail.com>
2024-12-12 20:05:09 +0100

    Change distro url

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://abyssoria.fr/storage/data/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://abyssoria.axeno.me/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
